1181e260664846a0451b19558c2b9c8d22fe8bdf
James Pak <jamesjiminpak@gmail.com>
2019-05-03 09:46:03 -0700

    filtercards function written

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -28,7 +28,7 @@ export default class Content extends Component {
     // this function should take in the tab and update the state with the new tab.
     
     this.setState ({
-      tabs: tab
+      selected: tab
     })
 
   };
@@ -36,7 +36,7 @@ export default class Content extends Component {
   filterCards = () => {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
+      by using it to filter out our cards for when a tab is selected
       
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
@@ -46,7 +46,16 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+
+   if (this.state.selected === 'all') {
     return this.state.cards;
+   } else {
+     return this.state.cards.filter((card) => {
+       return card.tab === this.state.selected;
+     })
+   }
+
+    
   };
 
   render() {
@@ -57,7 +66,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
         <Cards cards={this.filterCards()} />
       </div>
     );
