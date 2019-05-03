555f5a9e8d8809f7f869e3758e3aaa9eab8b3f5e
James Pak <jamesjiminpak@gmail.com>
2019-05-03 10:56:00 -0700

    selectTabHandler fixed, IT'S WORKING

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,10 +4,13 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+      const onClickHandler = e => props.selectTabHandler(props.tab)
+
   return (
     <div
       className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
-      onClick={props.changeSelected}
+      onClick={ onClickHandler }
       >
         
         {/* Replace this dummy click handler function with your selectTabHandler function from props 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -11,7 +11,7 @@ const Tabs = props => {
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
 
             {props.tabs.map( tab => {
-              return <Tab tab={tab} selectedTab={props.selected} selectTabHandler={props.changeSelected} />
+              return <Tab tab={tab} selectedTab={props.selected} selectTabHandler={props.selectTabHandler} />
             })}
 
       </div>
